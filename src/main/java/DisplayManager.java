ced0e24af8aded1f11b4d82837208cc10c6db763
Sam Behm <sambehmwork@gmail.com>
2023-02-26 19:01:40 +1000

    First Commit

---
+++ src/main/java/DisplayManager.java
@@ -0,0 +1,17 @@
+import org.lwjgl.opengl.
+
+public class DisplayManager {
+
+    public static void createDisplay() {
+
+    }
+
+    public static void updateDisplay() {
+
+    }
+
+    public static void destroyDisplay() {
+
+    }
+
+}
